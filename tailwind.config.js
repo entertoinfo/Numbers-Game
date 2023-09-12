c34b5859d460082a13dca10ee290baa6253b21ac
Igor Farafonov <entertoinfo29@gmail.com>
2023-09-12 21:53:05 +0300

    Revert "0.1"
    This reverts commit 6777c4a0cb4a8e26338a3da495e8c2a841e7ffd1.

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,18 +2,7 @@
 module.exports = {
   content: ["./*.{html,js}"],
     theme: {
-      extend: {
-        colors: {
-          'primary': '#22223B',
-          'secondary': '#9A8C98',
-          'text': '#C9ADA7',
-        },
-        fontFamily: {
-          'fixel-bold': 'fixel-bold',
-          'fixel-normal': 'fixel-normal',
-          'fixel-regular': 'fixel-regular'
-        },
-      },
+      extend: {},
     },
     plugins: [],
   }
