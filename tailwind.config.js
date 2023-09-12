256ec780f70e5e48dcbbca9d006fc85ff8ebf89f
Igor Farafonov <entertoinfo29@gmail.com>
2023-09-12 21:58:27 +0300

    0.1

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./*.{html,js}"],
     theme: {
-      extend: {},
+      extend: {
+        colors: {
+          'primary': '#22223B',
+          'secondary': '#9A8C98',
+          'text': '#C9ADA7',
+        },
+        fontFamily: {
+          'fixel-bold': 'fixel-bold',
+          'fixel-normal': 'fixel-normal',
+          'fixel-regular': 'fixel-regular'
+        },
+      },
     },
     plugins: [],
   }
