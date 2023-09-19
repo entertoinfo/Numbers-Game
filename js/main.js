7efd8b2b762a934771534880504c939811da16ab
Igor Farafonov <entertoinfo29@gmail.com>
2023-09-19 22:36:02 +0300

    modules

--- js/main.js
+++ js/main.js
@@ -1,23 +1,3 @@
-import fiveNumbers from "./modules/one-zero.mjs";
+import { createTable, numbersTable } from "./modules/one-zero.js";
 
-let container = document.getElementById("tile");
-
-// Перебираємо масив та вставляємо кожен блок цифр
-for (let i = 0; i < fiveNumbers.length; i++) {
-  var number = fiveNumbers[i];
-
-  // Створюємо елемент для блока
-  let gridItem = document.createElement("div");
-  gridItem.className = "bg-secondary m-2 flex justify-center items-center text-text font-fixel-regular text-2xl";
-
-  // Створюємо елемент <p> та вставляємо цифру.
-  let paragraph = document.createElement("p");
-  paragraph.id = "number";
-  paragraph.textContent = number;
-
-  // Додаємо <p> в блок
-  gridItem.appendChild(paragraph);
-
-  // Додаємо <div> в контейнер
-  container.appendChild(gridItem);
-}
+createTable(numbersTable);
--- js/modules/one-zero.mjs
+++
@@ -1,6 +0,0 @@
-const fiveNumbers = [
-  8, 18, 4, 14, 6, 26, 16, 20, 28, 22, 2, 30, 12, 10, 24, 6, 8, 16, 12, 26, 24,
-  28, 14, 18, 30,
-];
-
-export default fiveNumbers;
