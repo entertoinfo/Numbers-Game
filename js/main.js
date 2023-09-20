61bb171813a2cbdda79e73062de311e260aa78ad
Igor Farafonov <entertoinfo29@gmail.com>
2023-09-21 01:56:08 +0300

    fix 0.2

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
-import { createTable, numbersTable } from "./modules/one-zero.js";
+const numbers = [
+    8, 18, 4, 14, 6, 26, 16, 20, 28, 22, 2, 30, 12, 10, 24, 6, 8, 16, 12, 26, 24,
+    28, 14, 18, 30,
+  ];
 
-createTable(numbersTable);
+  myModule(numbers);
---
+++ js/one-zero.js
@@ -0,0 +1,25 @@
+function myModule(numbers) {
+  let container = document.getElementById("tile");
+
+  // Перебираємо масив та вставляємо кожен блок цифр
+  for (let i = 0; i < numbers.length; i++) {
+    var number = numbers[i];
+
+    // Створюємо елемент для блока
+    let gridItem = document.createElement("div");
+    gridItem.className =
+      "bg-secondary m-2 flex justify-center items-center text-text font-fixel-regular text-2xl";
+    gridItem.id = "number " + [i];
+
+    // Створюємо елемент <p> та вставляємо цифру.
+    let paragraph = document.createElement("p");
+    paragraph.id = "number" + [i];
+    paragraph.textContent = number;
+
+    // Додаємо <p> в блок
+    gridItem.appendChild(paragraph);
+
+    // Додаємо <div> в контейнер
+    container.appendChild(gridItem);
+  }
+};
