c4cbe873a7240ff2db4e57194862b2e779b077a4
entertoinfo <entertoinfo29@gmail.com>
2023-10-01 15:58:44 +0300

    0.2

--- js/main.js
+++ js/main.js
@@ -1,19 +1,46 @@
-const numbers = [
-    8, 18, 4, 14, 6, 26, 16, 20, 28, 22, 2, 30, 12, 10, 24, 6, 8, 16, 12, 26, 24,
-    28, 14, 18, 30,
-  ];
+// const numbers = [
+//     8, 18, 4, 14, 6, 26, 16, 20, 28, 22, 2, 30, 12, 10, 24, 6, 8, 16, 12, 26, 24,
+//     28, 14, 18, 30,
+//   ];
 
+ // Створюємо порожній масив для зберігання пар.
+const pairs = []; 
+
+function generateRandomPairs(count) {
+  // Спочатку ми створюємо масив чисел від 1 до count.
+  const numbers = Array.from({ length: count }, (_, i) => i + 1);
+
+  // Поки масив чисел не пустий...
+  while (numbers.length > 0) {
+    // Генеруємо випадковий індекс з чисел, які залишилися в масиві.
+    const randomIndex = Math.floor(Math.random() * numbers.length);
+    // Видаляємо число з масиву чисел і зберігаємо його значення.
+    const number = numbers.splice(randomIndex, 1)[0];
+    // Додаємо це число двічі до масиву пар.
+    pairs.push(number, number);
+  }
+
+  // Повертаємо масив пар.
+  return pairs;
+}
+
+//! тут трохи дивно працює, тому що тре вказати тут вапу і в майні задати кількість.
+//! і виходить умосно що в мейні ми задаємо 8, а тут ще створюється пара тобто виходить 10 цифр.
+const numberOfPairs = 1; // Кількість пар, яку ми хочемо створити.
+const pairsArray = generateRandomPairs(numberOfPairs); // Викликаємо функцію для генерації пар.
+
+console.log(pairsArray); // Виводимо результат у консоль.
 
   const shuffle = (numbers) => {
-    let m = numbers.length, t, i;
+    let numLength = numbers.length, a, b;
   
-    while (m) {
+    while (numLength) {
   
-      i = Math.floor(Math.random() * m--);
+      b = Math.floor(Math.random() * numLength--);
   
-      t = numbers[m];
-      numbers[m] = numbers[i];
-      numbers[i] = t;
+      a = numbers[numLength];
+      numbers[numLength] = numbers[b];
+      numbers[b] = a;
     }
   
     return numbers;
@@ -21,4 +48,4 @@ const numbers = [
 
   generateRandomPairs(14)
   let gameContainer = document.getElementById("game");
-  gameContainer.appendChild(fieldСreation(shuffle(pairsArray)));
+  gameContainer.appendChild(oneZero(shuffle(pairsArray)));
--- js/one-zero.js
+++ js/one-zero.js
@@ -1,4 +1,4 @@
-function fieldСreation(numbers) {
+function oneZero(numbers) {
   //Створення області для цифр
   let gridContainer = document.createElement("div");
   gridContainer.className = "grid grid-cols-5 w-[400px] h-[400px] border-4 rounded-sm border-secondary";
@@ -15,33 +15,3 @@ function fieldСreation(numbers) {
   }
   return gridContainer;
 }
-
-
-  
-// Створюємо порожній масив для зберігання пар.
-const pairs = []; 
-
-function generateRandomPairs(count) {
-  // Спочатку ми створюємо масив чисел від 1 до count.
-  const numbers = Array.from({ length: count }, (_, i) => i + 1);
-
-  // Поки масив чисел не пустий...
-  while (numbers.length > 0) {
-    // Генеруємо випадковий індекс з чисел, які залишилися в масиві.
-    const randomIndex = Math.floor(Math.random() * numbers.length);
-    // Видаляємо число з масиву чисел і зберігаємо його значення.
-    const number = numbers.splice(randomIndex, 1)[0];
-    // Додаємо це число двічі до масиву пар.
-    pairs.push(number, number);
-  }
-
-  // Повертаємо масив пар.
-  return pairs;
-}
-
-//! тут трохи дивно працює, тому що тре вказати тут вапу і в майні задати кількість.
-//! і виходить умосно що в мейні ми задаємо 8, а тут ще створюється пара тобто виходить 10 цифр.
-const numberOfPairs = 1; // Кількість пар, яку ми хочемо створити.
-const pairsArray = generateRandomPairs(numberOfPairs); // Викликаємо функцію для генерації пар.
-
-console.log(pairsArray); // Виводимо результат у консоль.
