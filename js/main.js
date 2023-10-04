b96714e219a3a543deba0948d088e7647cab7dfe
Igor Farafonov <entertoinfo29@gmail.com>
2023-10-04 18:15:57 +0300

    0.4

--- js/main.js
+++ js/main.js
@@ -1,46 +1,32 @@
- // Створюємо порожній масив для зберігання пар.
-const pairs = []; 
-
+const pairs = [];
 function generateRandomPairs(count) {
-  // Спочатку ми створюємо масив чисел від 1 до count.
   const numbers = Array.from({ length: count }, (_, i) => i + 1);
 
-  // Поки масив чисел не пустий...
   while (numbers.length > 0) {
-    // Генеруємо випадковий індекс з чисел, які залишилися в масиві.
     const randomIndex = Math.floor(Math.random() * numbers.length);
-    // Видаляємо число з масиву чисел і зберігаємо його значення.
     const number = numbers.splice(randomIndex, 1)[0];
-    // Додаємо це число двічі до масиву пар.
+
     pairs.push(number, number);
   }
-
-  // Повертаємо масив пар.
   return pairs;
 }
-
-//! тут трохи дивно працює, тому що тре вказати тут вапу і в майні задати кількість.
-//! і виходить умосно що в мейні ми задаємо 8, а тут ще створюється пара тобто виходить 10 цифр.
 const numberOfPairs = 1; // Кількість пар, яку ми хочемо створити.
-const pairsArray = generateRandomPairs(numberOfPairs); // Викликаємо функцію для генерації пар.
+const pairsArray = generateRandomPairs(numberOfPairs);
 
-console.log(pairsArray); // Виводимо результат у консоль.
+const shuffle = (numbers) => {
+  let numLength = numbers.length;
+  let firstIndex, nextIndex;
 
-  const shuffle = (numbers) => {
-    let numLength = numbers.length, a, b;
-  
-    while (numLength) {
-  
-      b = Math.floor(Math.random() * numLength--);
-  
-      a = numbers[numLength];
-      numbers[numLength] = numbers[b];
-      numbers[b] = a;
-    }
-  
-    return numbers;
+  while (numLength) {
+    nextIndex = Math.floor(Math.random() * numLength--);
+
+    firstIndex = numbers[numLength];
+    numbers[numLength] = numbers[nextIndex];
+    numbers[nextIndex] = firstIndex;
   }
+  return numbers;
+};
 
-  generateRandomPairs(14)
-  let gameContainer = document.getElementById("game");
-  gameContainer.appendChild(oneZero(shuffle(pairsArray)));
+generateRandomPairs(14);
+let gameContainer = document.getElementById("game");
+gameContainer.appendChild(oneZero(shuffle(pairsArray)));
