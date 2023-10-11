f30b873c109bc3b6b8a3e742a0fc158beb4710cb
Igor Farafonov <entertoinfo29@gmail.com>
2023-10-11 22:20:46 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
 
 // Кількість пар які створяться для ігрового поля.
-generateRandomPairs(15);
-let gameContainer = document.getElementById("game");
-gameContainer.appendChild(oneZero(shuffle(pairsArray)));
+oneZero(15);
