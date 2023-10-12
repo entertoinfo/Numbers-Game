63db54ae94c892e3fc0154e8ce750f6e8d308fdd
Igor Farafonov <entertoinfo29@gmail.com>
2023-10-13 01:59:25 +0300

    fix 0.1
    це котрий там вже фікс 😂

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+
+// Кількість пар які створяться для ігрового поля.
+const gameContainer = document.getElementById("game");
+      gameContainer.appendChild(oneZero(15));
+
+//   gameContainer.appendChild(oneZero(shuffle(pairsArray)));
